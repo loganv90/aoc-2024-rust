7402265f859c44ac905affaf07a52479914743c9
Logan Vaughan <logan.v90@gmail.com>
2024-12-03 00:49:21 -0700

    day01

---
+++ src/day02/solution.rs
@@ -0,0 +1,88 @@
+use std::fs;
+use std::env;
+use std::collections::HashMap;
+use crate::Day;
+
+pub struct Day02 {}
+
+impl Day for Day02 {
+    fn execute(&self) {
+        println!("Day 2");
+
+        let base_path = env::current_dir().unwrap();
+        let mut test1_path = base_path.clone();
+        test1_path.push("src/day02/test1");
+        let mut input1_path = base_path.clone();
+        input1_path.push("src/day02/input1");
+
+        part1(fs::read_to_string(&test1_path).unwrap());
+        part1(fs::read_to_string(&input1_path).unwrap());
+
+        part2(fs::read_to_string(&test1_path).unwrap());
+        part2(fs::read_to_string(&input1_path).unwrap());
+    }
+}
+
+fn part1(contents: String) {
+    let lines = contents.lines();
+
+    let mut left_list = vec![];
+    let mut right_list = vec![];
+
+    for line in lines {
+        let line_split = line.split(" ").filter(|x| x.len() > 0).collect::<Vec<&str>>();
+        if line_split.len() != 2 {
+            panic!("Invalid input");
+        }
+
+        let left_num = line_split[0].parse::<i64>().unwrap();
+        left_list.push(left_num);
+
+        let right_num = line_split[1].parse::<i64>().unwrap();
+        right_list.push(right_num);
+    }
+
+    left_list.sort();
+    right_list.sort();
+
+    let mut diff = 0;
+    for i in 0..left_list.len() {
+        let left_num = left_list[i];
+        let right_num = right_list[i];
+
+        diff += (left_num - right_num).abs();
+    }
+
+    println!("Part 1: {}", diff);
+}
+
+fn part2(contents: String) {
+    let lines = contents.lines();
+
+    let mut left_list = vec![];
+    let mut right_map = HashMap::new();
+
+    for line in lines {
+        let line_split = line.split(" ").filter(|x| x.len() > 0).collect::<Vec<&str>>();
+        if line_split.len() != 2 {
+            panic!("Invalid input");
+        }
+
+        let left_num = line_split[0].parse::<i64>().unwrap();
+        left_list.push(left_num);
+
+        let right_num = line_split[1].parse::<i64>().unwrap();
+        match right_map.get(&right_num) {
+            Some(cur) => { right_map.insert(right_num, cur + 1); },
+            None => { right_map.insert(right_num, 1); },
+        }
+    }
+
+    let mut res = 0;
+    for left in left_list {
+        res += right_map.get(&left).unwrap_or(&0) * left;
+    }
+
+    println!("Part 2: {}", res);
+}
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
-fn main() {
-    println!("Hello, world!");
+pub mod day01;
+pub mod day02;
+
+trait Day {
+    fn execute(&self);
 }
+
+fn main() {
+    let args: Vec<String> = std::env::args().collect();
+    if args.len() < 2 {
+        println!("Usage: cargo run -- <day: int>");
+        return;
+    }
+
+    let day_string = &args[1];
+    let day_int: i64 = match day_string.parse::<i64>() {
+        Ok(n) => n,
+        Err(_) => {
+            println!("Error: day must be an integer");
+            return;
+        },
+    };
+
+    if day_int < 1 || day_int > 25 {
+        println!("Error: day must be between 1 and 25");
+        return;
+    }
+
+    let days: Vec<Box<dyn Day>> = vec![
+        Box::new(crate::day01::solution::Day01 {}),
+        Box::new(crate::day02::solution::Day02 {}),
+    ];
+
+    let day = &days[(day_int - 1) as usize];
+    day.execute();
+}
+
